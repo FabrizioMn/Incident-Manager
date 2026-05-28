0ec89c3d41caa2dc57bbc853b5f92b942e0e45b0
FabrizioMn <Fabriziomn3@gmail.com>
2026-05-28 14:22:53 -0500

    Se añadio el Controlador para el servicio de la entidad Project con sus respectivos metodos

---
+++ backend/incident-manager/src/main/java/com/grupo01/incident_manager/controller/ProjectController.java
@@ -0,0 +1,47 @@
+package com.grupo01.incident_manager.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grupo01.incident_manager.dtos.project.ProjectRequest;
+import com.grupo01.incident_manager.dtos.project.ProjectResponse;
+import com.grupo01.incident_manager.service.ProjectService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<ProjectResponse> createProject(@Valid @RequestBody ProjectRequest request) {
+        return ResponseEntity.ok(projectService.createProject(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjectResponse>> getAllProjects() {
+        return ResponseEntity.ok(projectService.getAllProjects());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjectResponse> getProjectById(@PathVariable Long id) {
+        return ResponseEntity.ok(projectService.getProjectById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        projectService.deleteProject(id);
+        return ResponseEntity.noContent().build();
+    }
+}
